67028c7e96a693ac7efbb5dbda5c67f17c463b1b
chydikO <oleg.chudnovskiy@gmail.com>
2019-01-16 15:36:39 +0200

    1.3. JavaDoc. Code convention[#103569]

--- chapter_001/src/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/ru/job4j/calculate/Calculate.java
@@ -1,12 +1,28 @@
 package ru.job4j.calculate;
 
+/**
+ * Calculate
+ * @author Oleg Chudnovskij (oleg.chudnovskiy@gmail.com)
+ *  
+ */
+
 public class Calculate {
 
-    public String echo(String value) {
+    /**
+     * Method Echo
+     * @param value - input string for string format
+     * @return string with format
+     */
+    public String Echo(String value) {
         return String.format("%s %s %s", value, value, value);
     }
+    
+    /**
+     * Main.
+     * @param args - input args for calculate
+     */
     public static void main(String[] args) {
         Calculate calculate = new Calculate();
-        System.out.println(calculate.echo("Hi"));
+        System.out.println(calculate.Echo("Hi"));
     }
 }
