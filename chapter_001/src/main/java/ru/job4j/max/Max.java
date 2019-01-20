6cb73aaab3cb84af2edffe4ebcc5691cf7eec76a
chydikO <oleg.chudnovskiy@gmail.com>
2019-01-20 13:38:40 +0200

    4.2. Максимум из двух чисел[#103534]

---
+++ chapter_001/src/main/java/ru/job4j/max/Max.java
@@ -0,0 +1,14 @@
+package ru.job4j.max;
+
+public class Max {
+
+    /**
+     * Function Max Number
+     * @param i number 1
+     * @param i1 number 2
+     * @return Max Number
+     */
+    public int max(int i, int i1) {
+        return i > i1 ? i : i1;
+    }
+}
