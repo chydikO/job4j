47bae7f5d85ba190d80848c2e9b4a3bb0a4d1c9e
T510 <oleg.chudnovskiy@gmail.com>
2019-03-14 17:50:56 +0200

    2. Реализовать класс Tracker[#103624]_Maven problem

--- chapter_002/src/test/java/ru/job4j/tracker/TrackerTest.java
+++ chapter_002/src/test/java/ru/job4j/tracker/TrackerTest.java
@@ -1,5 +1,18 @@
 package ru.job4j.tracker;
 
-public class TrackerTest {
+import org.junit.Test;
 
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+class TrackerTest {
+    @Test
+    public void whenAddNewItemThenTrackerHasSameItem() {
+        Tracker tracker = new Tracker();
+        long created = System.currentTimeMillis();
+        Item item = new Item("test1","testDescription",created);
+        tracker.add(item);
+        Item result = tracker.findById(item.getId());
+        assertThat(result.getName(), is(item.getName()));
+    }
 }
