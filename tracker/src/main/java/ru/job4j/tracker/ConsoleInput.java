247faa07678fe2204ccbdfa4a097f5e90b51db95
T510 <oleg.chudnovskiy@gmail.com>
2019-04-08 14:50:21 +0300

    1. Используя класс ConsoleInput в классе StartUI обеспечить полноценную работу всего приложения[#103607]

--- tracker/src/main/java/ru/job4j/tracker/ConsoleInput.java
+++ tracker/src/main/java/ru/job4j/tracker/ConsoleInput.java
@@ -9,10 +9,6 @@ import java.util.Scanner;
 public class ConsoleInput implements Input {
     private Scanner scanner = new Scanner(System.in);
 
-    public ConsoleInput() {
-
-    }
-
     @Override
     public String ask(String s) {
         System.out.println(s); //печатаем вопрос у пользователя
