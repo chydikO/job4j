aa937b058a13eb7d31eba4588b8b3c01e4f2309d
chydikO <oleg.chudnovskiy@gmail.com>
2019-11-17 19:36:38 +0200

    7. Шаблон проектирования - Стратегия. [#786]

---
+++ chapter_002/src/test/ru.job4j/tracker/strategy/SquareTest.java
@@ -0,0 +1,24 @@
+package ru.job4j.tracker.strategy;
+import org.junit.Test;
+import ru.job4j.stragery.Square;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class SquareTest {
+    @Test
+    public void whenDrawSquare() {
+        Square square = new Square();
+        assertThat(
+                square.draw(),
+                is(
+                        new StringBuilder()
+                                .append("++++")
+                                .append("+     +")
+                                .append("+     +")
+                                .append("++++")
+                                .toString()
+                )
+        );
+    }
+}
